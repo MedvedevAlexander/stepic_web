da2e6f99f73fdbc415e53caabaec214b29d55e9b
Alexander Medvedev <alexander.medvedev1618@gmail.com>
2021-02-16 23:49:46 +0600

    step 2.1

--- init.sh
+++ init.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
 sudo cp ~/stepic_web/nginx.conf /etc/nginx/nginx.conf
+sudo cp ~/stepic_web/web /home/box/web
 sudo /etc/init.d/nginx start
 sudo nginx -s reload
-mkdir -p ~/web/public/img
-mkdir ~/web/public/css
-mkdir ~/web/public/js
-mkdir ~/web/uploads
-mkdir ~/web/etc
-cp ~/stepic_web/hello.py ~/web/hello.py
+#cp ~/stepic_web/hello.py ~/web/hello.py
