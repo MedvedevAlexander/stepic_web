db4d0400a6e4bb9492d8c1f1806c97c1c5c4945a
Alexander Medvedev <alexander.medvedev1618@gmail.com>
2021-02-17 00:20:56 +0600

    step 2.1

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+#sudo pip3 install django==2.0
 sudo cp ~/stepic_web/nginx.conf /etc/nginx/nginx.conf
-sudo cp ~/stepic_web/web /home/box/web
+sudo cp -R ~/stepic_web/web ~/web
 sudo /etc/init.d/nginx start
 sudo nginx -s reload
 #cp ~/stepic_web/hello.py ~/web/hello.py
