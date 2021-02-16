0274f56f709e0dea937dd7f3b3c523f825fcf539
Alexander Medvedev <alexander.medvedev1618@gmail.com>
2021-02-17 00:38:58 +0600

    step 2.1

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
-#sudo pip3 install django==2.0
+#обновляем только django хотя бы до версии 2.0, остальное не трогаем. Степ 2.1 таким образом был успешно сдан.
+#как же ужасен этот терминал
+
+sudo pip3 install django==2.0
 sudo cp ~/stepic_web/nginx.conf /etc/nginx/nginx.conf
 sudo cp -R ~/stepic_web/web ~/web
 sudo /etc/init.d/nginx start
 sudo nginx -s reload
 #cp ~/stepic_web/hello.py ~/web/hello.py
+cd ~/web/ask
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+k9zbf&exw774eycn*2ikeg5=w6)+6ro1m!z1j776d-^0t1-(z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
